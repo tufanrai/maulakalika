15f7835c434110d74eca0f3622db07c95faf4f09
TufanRai <thulungtufan16@gmail.com>
2025-11-30 13:42:09 +0545

    update: added title and description tags

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -2,21 +2,13 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchSpecificFile } from "@/app/api/download.api";
-import { useRouter } from "next/navigation";
 
 const page = () => {
-  const router = useRouter();
-
-  const { data, isLoading, isSuccess } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["fetch the url"],
     queryFn: fetchSpecificFile,
   });
 
-  useEffect(() => {
-    router.refresh();
-  }, [isSuccess]);
-
-  console.log(data?.file?.url);
   return (
     <div className="w-full flex flex-col justify-center">
       {/* body */}
@@ -29,11 +21,19 @@ const page = () => {
               </p>
             </div>
           ) : (
-            <embed
-              src={data?.file?.url}
-              type="application/pdf"
-              className="w-full h-200 bg-black"
-            ></embed>
+            <>
+              <h1 className="text-3xl md:text-4xl font-bold text-slate-900 mb-4">
+                {data?.file?.title}
+              </h1>
+              <p className="text-sm text-slate-600 leading-relaxed mb-4">
+                {data?.file?.description}
+              </p>
+              <embed
+                src={data?.file?.url}
+                type="application/pdf"
+                className="w-full h-200 bg-black"
+              ></embed>
+            </>
           )}
         </div>
       </section>
